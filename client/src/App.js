9b2938df29aabd036ac7fee991ff9ad8d8b43bb8
Pasindu Peiris <it21831454@my.sliit.lk>
2024-07-12 10:53:09 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Nav from '../src/components/Navigation.tsx'
-import Home from './pages/Home.tsx';
+import Nav from '../src/components/Navigation'
+import Home from './pages/Home';
 
 
 function App() {
