1b3a442c82b106e669b9cf2cdca9f58c16717b61
Pasindu Peiris <it21831454@my.sliit.lk>
2024-07-12 10:52:55 +0530

    Create Home.jsx

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import Logo from '../asset/Images/logo.png';
+import { BiPaste } from "react-icons/bi";
+import { BiQuestionMark } from "react-icons/bi";
+
+const Home = () => {
+    return (
+        <div className='flex items-center justify-center w-[100%] h-[80vh]'>
+
+            <div className="content-box">
+
+                <div className="grid items-center justify-center justify-items-center">
+                    <img src={Logo} alt="Logo" className="w-[240px]" />
+                    <h3 className=' font-bold text-xl mt-2'>Facebook Video Downloader</h3>
+                    <p className=' text-sm font-semibold text-slate-400 mt-1'>Download Facebook Videos Online</p>
+                </div>
+
+
+                <div className="flex items-center justify-center mt-4">
+
+                    <div className="rounded-md p-5">
+                        <div className="flex border-2 border-[#275eea] rounded-md">
+
+                            <input type="text" className="w-[500px]  border-[#275eea] rounded-md  bg-white p-4 text-base  outline-0  " placeholder="Enter Video Link Here..." id="" />
+                            <div className="flex w-10 items-center justify-center rounded-tl-md rounded-bl-md border-r border-[#9a9c9fbf] ">
+                                <BiPaste className=' text-xl text-slate-600' />
+                            </div>
+                            <input type="button" value="Download" className="bg-[#275eea] p-2 w-[120px] text-white font-semibold hover:bg-blue-800 transition-colors" />
+
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className=" flex items-center justify-center justify-items-center p-2">
+                    <button className="flex items-center justify-center justify-items-center bg-[#181e2d] p-2 w-[auto] text-white font-semibold text-sm hover:bg-[#275eea] transition-colors rounded-sm">    How to use FSAVE.net <BiQuestionMark className=' text-xl font-bold'/> </button>
+                </div>
+
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Home
