c7d759e880deccabf57e85cbfb85bb028f0a7b3c
Pasindu Peiris <it21831454@my.sliit.lk>
2024-07-13 18:18:42 +0530

    Update tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{jsx,js,tsx,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xsm': {'min': '200px', 'max': '640px'},
+      },
+    },
   },
   plugins: [],
 }
