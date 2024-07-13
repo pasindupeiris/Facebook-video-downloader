79ef52dce5efb10146de6ac9a71d9c087918b388
Pasindu Peiris <it21831454@my.sliit.lk>
2024-07-13 18:21:24 +0530

    Update Navigation.jsx

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -5,6 +5,8 @@ import { BiCommentDetail } from "react-icons/bi";
 
 const Navigation = () => {
 
+  const style = 'xl:flex md:flex lg:flex sm:flex xsm:grid items-center justify-around w-[100%] text-white font-semibold gap-5 bg-[#275eea] xsm:absolute xsm:top-[5.9rem] xsm:gap-14 xsm:p-4 xsm:left-0';
+
   return (
 
     <div className='w-[100%] h-[12vh] bg-[#275eea]'>
@@ -17,7 +19,7 @@ const Navigation = () => {
 
         <div className="menu p-1">
 
-          <ul className=' xl:flex md:flex lg:flex sm:flex xsm:grid items-center justify-around w-[100%] text-white font-semibold gap-5 bg-[#275eea] xsm:absolute xsm:top-[5.9rem] xsm:gap-14 xsm:p-4 xsm:left-0'>
+          <ul className={style}>
             <li className=' xsm:p-2 flex items-center justify-center'><a href="/">About Us</a> </li>
             <li className=' xsm:p-2 flex items-center justify-center'>Contact Us <BiCommentDetail className=' mx-2 text-xl' /></li>
           </ul>
