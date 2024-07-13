0a3fbbd92bd78801a1f2cfad83a4b5761113ab7b
Pasindu Peiris <it21831454@my.sliit.lk>
2024-07-13 18:18:39 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -21,9 +21,9 @@ const Home = () => {
 
 
     return (
-        <div className='flex items-center justify-center w-[100%] h-[90vh]'>
+        <div className='p-2 flex items-center justify-center w-[100%] xl:h-[90vh] lg:h-[90vh] md:h-[80vh] sm:h-[80vh] xsm:h-[80vh] sm:mx-2 '>
 
-            <div className="content-box">
+            <div className="content-box xsm:w-[100%]">
 
                 <div className="grid items-center justify-center justify-items-center">
                     <img src={Logo} alt="Logo" className="w-[240px]" />
@@ -34,10 +34,11 @@ const Home = () => {
 
                 <div className="flex items-center justify-center mt-4">
 
-                    <div className="rounded-md p-5">
+                    <div className="rounded-md xl:p-5 md:p-2 xsm:p-1 sm:p-2 ">
+
                         <div className="flex border-2 border-[#275eea] rounded-md">
 
-                            <input type="text"  ref={inputRef} className="w-[500px]  border-[#275eea] rounded-md  bg-white p-4 text-base  outline-0  " placeholder="Enter Video Link Here..." id="" />
+                            <input type="text"  ref={inputRef} className="xl:w-[500px] md:w-[100%] xsm:w-[100%] sm:w-[100%]  border-[#275eea] rounded-md  bg-white p-4 text-base  outline-0  " placeholder="Enter Video Link Here..." id="" />
                             <div className="flex w-10 items-center justify-center rounded-tl-md rounded-bl-md border-r border-[#9a9c9fbf] ">
                                 <BiPaste className=' text-xl text-slate-600' onClick={handlePaste} />
                             </div>
@@ -48,7 +49,7 @@ const Home = () => {
 
                 </div>
 
-                <div className=" flex items-center justify-center justify-items-center p-2">
+                <div className=" md:mt-4 sm:mt-3 xsm:mt-3 flex items-center justify-center justify-items-center p-2">
                     <a href="#new" className="flex items-center justify-center justify-items-center bg-[#181e2d] p-2 w-[auto] text-white font-semibold text-sm hover:bg-[#275eea] transition-colors rounded-sm">    How to use FSAVE.net <BiQuestionMark className=' text-xl font-bold' /> </a>
                 </div>
 
