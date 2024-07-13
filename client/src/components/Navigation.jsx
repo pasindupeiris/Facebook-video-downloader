e3ffd98ff1156d6c8feb04995e93c2cedf538741
Pasindu Peiris <it21831454@my.sliit.lk>
2024-07-13 18:37:30 +0530

    Update Navigation.jsx

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from '../asset/Images/rmlogo.png'
 import { BiCommentDetail } from "react-icons/bi";
 
 
 const Navigation = () => {
 
-  const style = 'xl:flex md:flex lg:flex sm:flex xsm:grid items-center justify-around w-[100%] text-white font-semibold gap-5 bg-[#275eea] xsm:absolute xsm:top-[5.9rem] xsm:gap-14 xsm:p-4 xsm:left-0';
+
+  const [isOpen ,  setopen] = useState('xl:flex md:flex lg:flex sm:flex xsm:grid items-center justify-around w-[100%] text-white font-semibold gap-5 bg-[#275eea] xsm:absolute xsm:top-[5.9rem] xsm:gap-14 xsm:p-4 xsm:left-0');
+  
+  const hadleOpen = () => {
+    isOpen === 'xl:flex md:flex lg:flex sm:flex xsm:grid items-center justify-around w-[100%] text-white font-semibold gap-5 bg-[#275eea] xsm:absolute xsm:top-[5.9rem] xsm:gap-14 xsm:p-4 xsm:left-0'? setopen('xl:flex md:flex lg:flex sm:flex xsm:grid items-center justify-around w-[100%] text-white font-semibold gap-5 bg-[#275eea] xsm:absolute xsm:top-[5.9rem] xsm:gap-14 xsm:p-4 xsm:left-0 xsm:hidden') : setopen('xl:flex md:flex lg:flex sm:flex xsm:grid items-center justify-around w-[100%] text-white font-semibold gap-5 bg-[#275eea] xsm:absolute xsm:top-[5.9rem] xsm:gap-14 xsm:p-4 xsm:left-0');
+  }
+
 
   return (
 
@@ -17,9 +23,13 @@ const Navigation = () => {
           <img src={Logo} alt="" width={150} />
         </div>
 
+        <div className="">
+          <button onClick={hadleOpen}>click</button>
+        </div>
+
         <div className="menu p-1">
 
-          <ul className={style}>
+          <ul className={isOpen}>
             <li className=' xsm:p-2 flex items-center justify-center'><a href="/">About Us</a> </li>
             <li className=' xsm:p-2 flex items-center justify-center'>Contact Us <BiCommentDetail className=' mx-2 text-xl' /></li>
           </ul>
